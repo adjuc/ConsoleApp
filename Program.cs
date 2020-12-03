3fabf2ea9242c3f5fe0a3b6a478d3caa572d5ced
adjuc <anders@codeimprover.com>
2020-12-03 13:57:56 +0100

    Commit2

--- Program.cs
+++ Program.cs
@@ -10,8 +10,8 @@ namespace ConsoleAppDemo1
             CalculateSumClass C1 = new CalculateSumClass();
             int sum = C1.CalculateSumOf8(1,2,3,4,5,6,7,8);
             Console.WriteLine("Sum is " + sum.ToString()); 
-            Calculate Cal = new Calculate();
-            Console.Write("SumOf8:" + Cal.CalculateSumOf8(1,2,3,4,5,6,7,8).ToString() + "\nEnd of procedure\n");
+            ICalculate iCal = new Calculate();
+            Console.Write("SumOf8:" + iCal.CalculateSumOf8(1,2,3,4,5,6,7,8).ToString() + "\nEnd of procedure\n");
         }
     }
 }
