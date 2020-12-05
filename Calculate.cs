22fc9fa5b117373952cee9a4c4acdb927110f754
adjuc <anders@codeimprover.com>
2020-12-05 13:45:58 +0100

    Commit1 sum of 9

--- Calculate.cs
+++ Calculate.cs
@@ -8,6 +8,10 @@ namespace ConsoleAppDemo1
         {
             return(v1+v2+v3+v4+v5+v6+v7+v8);
         }
+        public int CalculateSumOf9(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9)
+        {
+            return(v1+v2+v3+v4+v5+v6+v7+v8+v9);
+        }
         public int CalculateProductOf2(int v1, int v2)
         {
             // forgot a comment
