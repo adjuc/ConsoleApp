1c8b3f1da349d6c04f14abc322fcc22a15178085
adjuc <anders@codeimprover.com>
2020-12-07 10:26:46 +0100

    SumOf9 in interface

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleAppDemo1
             ICalculate iCal = new Calculate();
             Console.Write("ProductOf2:" + iCal.CalculateProductOf2(2,4).ToString() + "\n");
             Console.Write("DivisionOf 8/4:" + iCal.CalculateDivision(8, 4).ToString()+ "\n");
+            Console.Write("SumOf9:" + iCal.CalculateSumOf9(1,2,3,4,5,6,7,8,9).ToString() + "\n");
             Console.Write("SumOf8:" + iCal.CalculateSumOf8(1,2,3,4,5,6,7,8).ToString() + "\nEnd of procedure\n");
         }
     }
