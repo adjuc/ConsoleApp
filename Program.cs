3843265b9cc070214ec99641badc06e01df2e75b
adjuc <anders@codeimprover.com>
2020-12-11 15:35:35 +0100

    Commit3

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleAppDemo1
             Console.WriteLine("SumClass sum of 10: " + C1.CalculateSumOf10(1,2,3,4,5,6,7,8,9,10).ToString()); 
             ICalculate iCal = new Calculate();
             Console.Write("ProductOf2:" + iCal.CalculateProductOf2(2,4).ToString() + "\n");
+            Console.Write("ProductOf4:" + iCal.CalculateProductOf4(1,2,3,4).ToString() + "\n");
             Console.Write("DivisionOf 8/4:" + iCal.CalculateDivision(8, 4).ToString()+ "\n");
             Console.Write("SumOf8:" + iCal.CalculateSumOf8(1,2,3,4,5,6,7,8).ToString() + "\n");
             Console.Write("SumOf9:" + iCal.CalculateSumOf9(1,2,3,4,5,6,7,8,9).ToString() + "\n");
