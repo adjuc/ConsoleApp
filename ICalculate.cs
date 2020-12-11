bc7dbdfca4a8f42da69a55415eb251e315a723b7
adjuc <anders@codeimprover.com>
2020-12-11 08:59:43 +0100

    Commit2

--- ICalculate.cs
+++ ICalculate.cs
@@ -4,7 +4,7 @@ namespace ConsoleAppDemo1
     {
         int CalculateSumOf8(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8);
         int CalculateSumOf9(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9);
-
+        public int CalculateSumOf10(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9, int v10);
         int CalculateProductOf2(int v1, int v2);   
         int CalculateDivision(int v1, int d2);         
     }
