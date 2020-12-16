c2d1a5ffea3b3d47aa4e563d49b5f12ef431aa64
adjuc <anders@codeimprover.com>
2020-12-16 09:40:50 +0100

    Commti1

--- Program.cs
+++ Program.cs
@@ -17,7 +17,9 @@ namespace ConsoleAppDemo1
             Console.Write("DivisionOf 8/4:" + iCal.CalculateDivision(8, 4).ToString()+ "\n");
             Console.Write("SumOf8:" + iCal.CalculateSumOf8(1,2,3,4,5,6,7,8).ToString() + "\n");
             Console.Write("SumOf9:" + iCal.CalculateSumOf9(1,2,3,4,5,6,7,8,9).ToString() + "\n");
-            Console.Write("SumOf10:" + iCal.CalculateSumOf10(1,2,3,4,5,6,7,8,9,10).ToString() + "\n");            
+            Console.Write("SumOf10:" + iCal.CalculateSumOf10(1,2,3,4,5,6,7,8,9,10).ToString() + "\n");  
+            LongMethodClass lc = new LongMethodClass();
+            Console.Write(lc.GetLongString("LongString: "));          
             Console.Write("End of procedure\n");
         }
     }
