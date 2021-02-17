f327b2f04a6c2cb1f4b78f408d2a856c3b0af3b9
Anders Djursaa <59800490+adjuc@users.noreply.github.com>
2021-02-17 10:09:49 +0100

    Update Test.cs

--- Test.cs
+++ Test.cs
@@ -4,8 +4,8 @@ namespace HelloWorld
 {
     public class CalculateClass
     {
-        int l1, l2, l3, l4, l5, l6, l7, l8, l9;
-        public CalculateClass(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9)
+        int l1, l2, l3, l4, l5, l6, l7, l8;
+        public CalculateClass(int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8)
         {
             l1 = v1;
             l2 = v2;
@@ -15,7 +15,7 @@ namespace HelloWorld
             l6 = v6;
             l7 = v7;
             l8 = v8;
-            l9 = v9;
+           
             if (l9 == 0)
             {
                 if (l1 > l2) 
@@ -91,7 +91,7 @@ namespace HelloWorld
         }
         public int Sum()
         {
-            return(l1+l2+l3+l4+l5+l6+l7+l8+l9);
+            return(l1+l2+l3+l4+l5+l6+l7+l8);
         }
     }
     
